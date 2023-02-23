42f3f49abeebc689868a3d465453611913567580
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-02-23 22:07:45 +0530

    WapperFunction

---
+++ Node_js/moduleWrapperFunction/index.js
@@ -0,0 +1,13 @@
+(     function(exports , require,modules,_filename,_dirname){
+   const a=require("fs");
+    const name = "akash";
+    console.log(name);
+    module.exports={akaahs}
+})
+
+
+
+
+
+
+
