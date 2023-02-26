ccdbaead2244c4ad20bf58ea7303bff50684ab93
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-02-26 23:40:13 +0530

    path

---
+++ Node_js/PATHmodule/path.js
@@ -0,0 +1,10 @@
+const path =require("path");
+
+console.log(path.dirname('D:/Node full course/OSmodule/osmod.js'));
+console.log(path.extname('D:/Node full course/OSmodule/osmod.js'))
+console.log(path.basename('D:/Node full course/OSmodule/osmod.js'))
+
+
+
+
+console.log(path.parse('D:/Node full course/OSmodule/osmod.js'))
